c3dab1eb6ae55f84636965f01648414d1e7b1532
jcall1 <jcall1@mail.csuchico.edu>
2019-09-23 16:05:07 -0700

    Gets users first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Justin Call
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
+  string firstname;
+  string lastname;
 
   //get user input
+  cout<<"What is your first name?"<<endl;
+  cin>>firstname;
+  cout<<"What is your last name?"<<endl;
+  cin>>lastname;
+  cout<<"Welcome, "<<firstname[0]<<"."<<lastname[0]<<", here is your fortune..."<<endl;
 
   //tell fortune
 
